0d4e13a860333c74b5231ec046bbfa01c5308e53
Gocha <Frost220v@gmail.com>
2023-05-24 20:53:17 +0500

    программа показывает максимальное число

---
+++ Seminar001_DZ02/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 4: Напишите программу, которая принимает 
+//на вход три числа и выдаёт максимальное из этих чисел.
+
+
+// Console.WriteLine("Введите первое число ");
+// int num=Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите второе число ");
+// int num2=Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите третье число ");
+// int num3=Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine(Math.Max(num, Math.Max(num2, num3)));
+
+Console.WriteLine("Введите первое число ");
+int num=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число ");
+int num2=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число ");
+int num3=Convert.ToInt32(Console.ReadLine());
+int max = num;
+if (num2 > num)
+    max = num2;
+if (num3 > max)
+    max = num3;
+Console.WriteLine("Найдено максимальное число ");
+Console.Write("Max = ");
+Console.Write(max);
