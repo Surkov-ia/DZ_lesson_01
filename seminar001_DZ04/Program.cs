9d2e94216c0a75cd73328108e296cb81d553976a
Gocha <Frost220v@gmail.com>
2023-05-24 20:54:04 +0500

    программа показывает все четные числа введеного числа

---
+++ seminar001_DZ04/Program.cs
@@ -0,0 +1,15 @@
+﻿//Задача 8: Напишите программу, которая на вход принимает число (N),
+// а на выходе показывает все чётные числа от 1 до N.
+
+Console.WriteLine("Введите число");
+int num=Convert.ToInt32(Console.ReadLine());
+int num1=1;
+while (num1 <= num)
+{
+    if (num1 % 2 == 0)
+    {
+        Console.WriteLine("Чётные числа");
+        Console.Write(num1 + ", ");
+    }
+    num1++;
+}
